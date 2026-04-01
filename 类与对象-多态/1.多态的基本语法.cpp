33a9d32321846c95169306f2389b3af82b08643f
twistvs <twistmahua@gmail.com>
2026-04-01 17:27:15 +0800

    4.1

--- 类与对象-多态/1.多态的基本语法.cpp
+++ 类与对象-多态/1.多态的基本语法.cpp
@@ -1,5 +1,8 @@
 ﻿# include <iostream>
 
+//1.有继承关系
+//2.子类重写父类的虚函数（重写指的是返回值，函数名和参数列表完全一致）
+//3.父类指针或引用指向子类对象
 class Father {
 public:
 	virtual void test() {
@@ -28,7 +31,9 @@ void test01(Father& f) {
 int main() {
 	Son1 s1;
 	Son2 s2;
-	test01(s1);
-	test01(s2);
+	test01(s1); //调用的Son1
+	test01(s2);   //调用的Son2
+
+	std::cout << sizeof(Father) << std::endl;  //输出为8，也就是一个指针的大小
 	return 0;
 }
